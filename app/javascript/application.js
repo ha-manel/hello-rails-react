436ce26064a30cf4e296f51acd4f0439835c3c6e
ha-manel <manelhammouche3@gmail.com>
2022-09-19 11:51:53 +0100

    Add react app

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Greeting from './Greetings/Greeting';
+
+function App() {
+  return (
+    <Greeting />
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
